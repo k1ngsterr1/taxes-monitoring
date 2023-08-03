f1e630d44818d092b1b6acae586433c505e989ef
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-08-03 19:39:03 +0600

    taxes-monitoring V1.5

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -53,16 +53,11 @@ const AboutScreen = () => {
                   </>
                 }
               >
-                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                non cupidatat skateboard dolor brunch. Food truck quinoa
-                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
-                put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                excepteur butcher vice lomo. Leggings occaecat craft beer
-                farm-to-table, raw denim aesthetic synth nesciunt you probably
-                haven't heard of them accusamus labore sustainable VHS.
+                В рамках обычной выездной проверки решение налогового органа
+                может быть получено по истечению 4-5 лет после окончания
+                налогового периода с соответствующими этому периоду суммой пени.
+                В налоговом мониторинге период проверки заканчивается в течение
+                9 месяцев после окончания налогового периода.
               </MDBAccordionItem>
             </MDBAccordion>
           </div>
