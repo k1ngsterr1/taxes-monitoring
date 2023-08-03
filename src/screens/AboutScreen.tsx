727e38d8ab930fa1d4d96d71d41c949cf813571b
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-08-03 18:59:48 +0600

    taxes-monitoring V1.3

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,6 +1,22 @@
 import React from "react";
+import "../styles/about/about-styles.css";
+
+import star from "../assets/star.svg";
+
+interface TabProps {
+  propText: string;
+}
 
 const AboutScreen = () => {
+  const Tab: React.FC<TabProps> = (props) => {
+    return (
+      <div className="tab">
+        <img className="star" src={star} alt="star"></img>
+        <p className="tab-text">{props.propText}</p>
+      </div>
+    );
+  };
+
   return (
     <div className="about-screen">
       <div className="container">
