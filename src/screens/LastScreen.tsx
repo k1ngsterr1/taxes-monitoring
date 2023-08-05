a9f3c34968d9b7b61af6c88b12f3aaaad56be55f
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-08-05 20:07:39 +0600

    taxes-monitoring V3.4

--- src/screens/LastScreen.tsx
+++ src/screens/LastScreen.tsx
@@ -6,52 +6,71 @@ import star from "../assets/star.svg";
 
 import "../styles/last/last-styles.css";
 
+const Fade = require("react-reveal/Fade");
+
 const LastScreen = () => {
   return (
     <div className="last-screen" id="last">
       <div className="container">
-        <div className="additional-text">
-          <p className="mini-text">Результаты</p>
-          <figure className="line"></figure>
-        </div>
-        <h2 className="heading">
-          Результаты внедрения решения <span>по налоговому мониторингу</span>
-        </h2>
+        <Fade bottom delay={100}>
+          <div className="additional-text">
+            <p className="mini-text">Результаты</p>
+            <figure className="line"></figure>
+          </div>
+        </Fade>
+        <Fade bottom delay={200}>
+          <h2 className="heading">
+            Результаты внедрения решения <span>по налоговому мониторингу</span>
+          </h2>
+        </Fade>
         <div className="taxes-monitoring-one">
-          <div className="star-container">
-            <img className="star" src={star} alt="star"></img>
-            <p className="star-text">
-              Индикатор высокого уровня автоматизации учетных процессов и
-              прозрачности бизнеса с налоговой точки зрения
-            </p>
-          </div>
-          <div className="star-container">
-            <img className="star" src={star} alt="star"></img>
-            <p className="star-text">
-              Уникальное на рынке РФ решение, встраиваемое в текущие ИС компании
-              и не зависящее от внешнего поставщика ПО
-            </p>
-          </div>
-          <div className="star-container">
-            <img className="star" src={star} alt="star"></img>
-            <p className="star-text">
-              Структурированное хранение первичных документов и договоров
-            </p>
-          </div>
-          <img
-            src={illustrationOne}
-            alt="illustration"
-            className="illustration-one"
-          />
+          <Fade bottom delay={300}>
+            <div className="star-container">
+              <img className="star" src={star} alt="star"></img>
+              <p className="star-text">
+                Индикатор высокого уровня автоматизации учетных процессов и
+                прозрачности бизнеса с налоговой точки зрения
+              </p>
+            </div>
+          </Fade>
+          <Fade bottom delay={400}>
+            <div className="star-container">
+              <img className="star" src={star} alt="star"></img>
+              <p className="star-text">
+                Уникальное на рынке РФ решение, встраиваемое в текущие ИС
+                компании и не зависящее от внешнего поставщика ПО
+              </p>
+            </div>
+          </Fade>
+          <Fade bottom delay={500}>
+            <div className="star-container">
+              <img className="star" src={star} alt="star"></img>
+              <p className="star-text">
+                Структурированное хранение первичных документов и договоров
+              </p>
+            </div>
+          </Fade>
+          <Fade bottom delay={600}>
+            <img
+              src={illustrationOne}
+              alt="illustration"
+              className="illustration-one"
+            />
+          </Fade>
         </div>
-        <figure className="line-2"></figure>
+        <Fade bottom delay={700}>
+          <figure className="line-2"></figure>
+        </Fade>
+
         <div className="taxes-monitoring-two">
           <div className="star-container">
-            <img src={star} alt="star" className="star" />
-            <p className="star-text">
-              База для совершенствования зависимых и кросс-зависимых процессов и
-              процедур в Группе
-            </p>
+            <Fade bottom delay={800}>
+              <img src={star} alt="star" className="star" />
+              <p className="star-text">
+                База для совершенствования зависимых и кросс-зависимых процессов
+                и процедур в Группе
+              </p>
+            </Fade>
           </div>
           <div className="star-container">
             <img src={star} alt="star" className="star" />
--- src/screens/ResultsScreen.tsx
+++ src/screens/ResultsScreen.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import star from "../assets/star.svg";
 import "../styles/results/results-styles.css";
 
+const Fade = require("react-reveal/Fade");
+
 interface StarTabProps {
   propsText: string;
 }
@@ -38,30 +40,56 @@ const ResultsScreen = () => {
   return (
     <div className="results-screen" id="results">
       <div className="container">
-        <div className="additional-text">
-          <p className="mini-text">Итоги</p>
-          <figure className="line"></figure>
-        </div>
-        <h2 className="heading">
-          Что вы получите по <span>итогам проекта</span>
-        </h2>
-        <StarTab propsText="Комплексное решение"></StarTab>
-        <StarTab propsText="Интеграцию с АИС Налог-3"></StarTab>
-        <StarTab propsText="Техническую поддержку решения"></StarTab>
-        <figure className="between-line"></figure>
-        <StarTabMedium propsText="Решение может взаимодействовать с любыми учтёнными системами"></StarTabMedium>
-        <StarTabMedium propsText="Легко тиражируемое решение на другие компании группы"></StarTabMedium>
-        <StarTabMedium propsText="Комплект методологических, регламентных и иных документов"></StarTabMedium>
-        <figure className="between-line"></figure>
-        <StarTabBig
-          propsText="Подсистему обработки запросов от налоговых инспекторов с отчетом о действиях
+        <Fade bottom delay={100}>
+          <div className="additional-text">
+            <p className="mini-text">Итоги</p>
+            <figure className="line"></figure>
+          </div>
+        </Fade>
+        <Fade bottom delay={200}>
+          <h2 className="heading">
+            Что вы получите по <span>итогам проекта</span>
+          </h2>
+        </Fade>
+        <Fade bottom delay={300}>
+          <StarTab propsText="Комплексное решение"></StarTab>
+        </Fade>
+        <Fade bottom delay={400}>
+          <StarTab propsText="Интеграцию с АИС Налог-3"></StarTab>
+        </Fade>
+        <Fade bottom delay={500}>
+          <StarTab propsText="Техническую поддержку решения"></StarTab>
+        </Fade>
+        <Fade bottom delay={600}>
+          <figure className="between-line"></figure>
+        </Fade>
+        <Fade bottom delay={700}>
+          <StarTabMedium propsText="Решение может взаимодействовать с любыми учтёнными системами"></StarTabMedium>
+        </Fade>
+        <Fade bottom delay={800}>
+          <StarTabMedium propsText="Легко тиражируемое решение на другие компании группы"></StarTabMedium>
+        </Fade>
+        <Fade bottom delay={900}>
+          <StarTabMedium propsText="Комплект методологических, регламентных и иных документов"></StarTabMedium>
+        </Fade>
+        <Fade bottom delay={1000}>
+          <figure className="between-line"></figure>
+        </Fade>
+        <Fade bottom delay={1100}>
+          <StarTabBig
+            propsText="Подсистему обработки запросов от налоговых инспекторов с отчетом о действиях
         пользователей"
-        ></StarTabBig>
-        <StarTabBig
-          propsText="Эффективную ролевую модель, с минимальным количеством ролей и конфликтов в
+          ></StarTabBig>
+        </Fade>
+        <Fade bottom delay={1200}>
+          <StarTabBig
+            propsText="Эффективную ролевую модель, с минимальным количеством ролей и конфликтов в
           решении"
-        ></StarTabBig>
-        <StarTabBig propsText="Модели раскрытия отчетности с надстроенными контрольными соотношениями"></StarTabBig>
+          ></StarTabBig>
+        </Fade>
+        <Fade bottom delay={1300}>
+          <StarTabBig propsText="Модели раскрытия отчетности с надстроенными контрольными соотношениями"></StarTabBig>
+        </Fade>
       </div>
     </div>
   );
