a570292a96a136df66bd76188146fec916131d07
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-08-07 00:55:58 +0600

    taxes-monitoring V4.3

--- src/screens/RequirementsScreen.tsx
+++ src/screens/RequirementsScreen.tsx
@@ -30,7 +30,7 @@ const RequirementsScreen = () => {
 
   return (
     <div className="requirements-screen" id="requirements">
-      <div className="container">
+      <div className="container-box">
         <Fade bottom>
           <h2 className="heading">
             Критерии для вступления в <span>налоговый мониторинг</span>
@@ -72,6 +72,39 @@ const RequirementsScreen = () => {
         <Fade bottom>
           <button className="request">Оставить заявку</button>
         </Fade>
+      </div>
+      <div className="container-box-pc">
+        <h2 className="heading">
+          Критерии для вступления в <span>налоговый мониторинг</span>
+        </h2>
+        <div className="tab">
+          <img src={star} alt="star" className="star" />
+          <p className="tab-text">
+            На этапе внедрения налогового мониторинга в России перейти на
+            налоговый мониторинг могли только самые крупные компании. Постепенно
+            критерии для получения права вхождения в налоговый мониторинг
+            смягчаются, и сейчас компания должна отвечать одновременно следующим
+            требованиям:
+          </p>
+        </div>
+        <div className="boxes">
+          <Box
+            propNumber="1"
+            propHeading="Критерий"
+            propText="<span><b>100 млн рублей</b></span> - общая сумма налогов за год"
+          ></Box>
+          <Box
+            propNumber="1"
+            propHeading="Критерий"
+            propText="1 млрд рублей - сумма полученных доходов организацией"
+          ></Box>
+          <Box
+            propNumber="1"
+            propHeading="Критерий"
+            propText="1 млрд рублей - совокупная стоимость активов."
+          ></Box>
+          <button className="request">Оставить заявку</button>
+        </div>
       </div>
     </div>
   );
